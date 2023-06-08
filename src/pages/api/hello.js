52ebdf884005cda3e034cfe6410ae9010fd552f2
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2023-06-08 22:23:01 +0100

    modbus

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,47 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import ModbusRTU from 'modbus-serial';
+
+const host = '192.168.43.184';
+const port = 502;
+
+export default async function handler(req, res) {
+  const { reg, val } = req.body.data;
+  const client = new ModbusRTU();
+  try {
+    await client.connectTCP(host, { port });
+
+    switch (Number(reg)) {
+      case 4003:
+        try {
+          const data = await client.readInputRegisters(Number(reg),1);
+          res.status(200).json({ value: data.data[0] });
+        } catch (err) {
+          console.error(err);
+          res
+            .status(500)
+            .send(`Error reading Modbus register(Humidity): ${err.message}`);
+        } finally {
+          client.close();
+        }
+        break;
+      case 4001:
+        try {
+          await client.writeCoil(reg, Number(val)).then(() => {
+            res.status(200).send(`Successfully wrote to Buzzer Coil`);
+          });
+        } catch (err) {
+          console.log(err);
+          res.status(500).send(`Could not write to Buzzer Coil`);
+        } finally {
+          client.close();
+        }
+        break;
+      default:
+        res.status(500).send(`Sorry!, please provide a valid input`);
+        client.close();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Error connecting to Modbus server: ${err.message}`);
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,120 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Modbus from 'jsmodbus';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [flag, setFlag] = useState(false);
+  const [hum, setHum] = useState(0);
+  const [inputValue, setInputValue] = useState({
+    reg: '',
+    val: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('/api/hello', { data: inputValue });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (inputValue.reg !== '' && inputValue.val !== '') {
+      fetchData();
+    }
+  }, [inputValue]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setInputValue((prev) => ({
+      ...prev,
+      val: event.target.value,
+    }));
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setInputValue((prev) => ({
+      ...prev,
+      reg: e.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('/api/hello', { data: inputValue });
+        console.log(response.data);
+        setHum(response.data.value);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (flag) {
+      fetchData();
+
+      setInterval(() => {
+        fetchData();
+      }, 5000);
+    }
+  }, [inputValue, flag]);
+
+  const handleHum = async ({ target }) => {
+    setInputValue((prev) => ({
+      ...prev,
+      reg: target.value,
+    }));
+    setFlag(true);
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex space-y-40 flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <form className="space-y-4 flex flex-col">
+        <div className="space-x-4">
+          <button
+            type="text"
+            value={4001}
+            className="border w-32 h-32 rounded-full"
+            onClick={handleInput}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Sound Buzzer
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          type="submit"
+          value={'1'}
+          onClick={handleSubmit}
+          className="border rounded-md px-4 "
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Start
+        </button>
+        <button
+          type="submit"
+          value={'0'}
+          onClick={handleSubmit}
+          className="border rounded-md px-4 "
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Stop
+        </button>
+      </form>
+      <div>
+        <button
+          type="text"
+          value={4003}
+          className="border w-32 h-32 rounded-full"
+          onClick={handleHum}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+         Get Humidity
+        </button>
+        <p className="text-center mt-3 text-lg">Humidity: { hum}</p>
       </div>
     </main>
-  )
+  );
 }
